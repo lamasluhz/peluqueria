f1b9dae36e7eddce2c23d9ae3f879f2928b7e859
KiraSan08 <118699417+KiraSan08@users.noreply.github.com>
2023-05-16 12:09:46 -0400

    Update index.js

--- src/Componentes/index.js
+++ src/Componentes/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from "./Header"
 export { default as Clientes } from './Clientes'
 export { default as Peluqueros } from './Peluqueros'
+export { default as Peluqueros } from './Calendario'
