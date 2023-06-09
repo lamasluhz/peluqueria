4981435b3d60b1551334f46f053f6fe253753d78
HamudiEZ <131073895+HamudiEZ@users.noreply.github.com>
2023-06-09 02:44:32 -0400

    Add files via upload

---
+++ src/Componentes/BuscadorVentaProductos.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState } from 'react';
+import { InputGroup, FormControl, Button } from "react-bootstrap";
+import { BsSearch, BsPerson } from "react-icons/bs";
+import { IoMdPersonAdd } from "react-icons/io";
+import { Form } from "react-bootstrap";
+
+
+
+const BuscadorVentaProductos = ({ action, handleSearch }) => {
+    const [searchValue, setSearchValue] = useState('');
+
+    const handleInputChange = (event) => {
+        setSearchValue(event.target.value);
+        handleSearch(event.target.value);
+    };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Perform any necessary actions on form submission
+        // For example, you can trigger a search or other operations
+    };
+
+    return (
+        <InputGroup>
+            <InputGroup.Text>
+                <BsSearch />
+            </InputGroup.Text>
+            <FormControl placeholder="Buscar" onChange={handleInputChange} value={searchValue} />
+        </InputGroup>
+    );
+};
+
+export default BuscadorVentaProductos;
--- src/Componentes/VentaProductos.js
+++ src/Componentes/VentaProductos.js
@@ -1,188 +1,206 @@
+import React, { useEffect, useState } from 'react';
+import { Container, Row, Col, Table, Button } from 'react-bootstrap';
+import { useLocation } from 'react-router-dom';
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import Buscador from './Buscador';
-import { Modal, Button } from 'react-bootstrap';
-import { MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
-import { FaTrash, FaPen, FaCheck, FaTimes } from 'react-icons/fa';
+import BuscadorCompraProductos from './BuscadorVentaProductos';
 
 const VentaProductos = () => {
-  const [ventas, setVentas] = useState([]);
+  const url = 'https://localhost:7137/StockProducto/GetStockProductos';
+  const location = useLocation();
+  const [proveedor, setProveedor] = useState();
   const [showModal, setShowModal] = useState(false);
-  const [searchQuery, setSearchQuery] = useState('');
-  const url = 'https://localhost:7137/api/TiposServicios/Lavado';
+  const [productos, setProducto] = useState([]);
+  const [productosSeleccionados, setProductoSeleccionados] = useState([]);
+  const [cantidadProducto, setCantidadProducto] = useState({});
+  const [productosFiltrados, setProductosFiltrados] = useState([]);
+
+  const { state } = location;
+
+  const obtenerProveedor = async () => {
+    if (!state || !state.idProveedor) {
+      // State or idProveedor is not defined, return early
+      return;
+    }
+
+    const response = await axios.get(`https://localhost:7137/Proveedor/${state.idProveedor}`);
+    setProveedor(response.data);
+  };
+
+  const obtenerProductos = async () => {
+    const response = await axios.get(url);
+    const updatedProductos = response.data.map((producto) => ({
+      ...producto,
+      precioUnitario: producto.precioUnitario + 10000,
+    }));
+    setProducto(updatedProductos);
+  };
 
   useEffect(() => {
-    obtenerVentas();
-  }, []);
+    obtenerProductos();
+  }, [proveedor]);
 
-  const obtenerVentas = () => {
-    axios
-      .get(url)
-      .then(response => {
-        setVentas(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+  useEffect(() => {
+    obtenerProveedor();
+  }, [state]); // Depend on state so the effect runs whenever state changes
+
+  const handleSearch = (searchValue) => {
+    if (searchValue.trim() === '') {
+      setProductosFiltrados([]);
+    } else {
+      const productosFiltrados = productos.filter(
+        (producto) =>
+          producto.nombre.toLowerCase().includes(searchValue.toLowerCase()) ||
+          producto.descripcionTipoProducto.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      setProductosFiltrados(productosFiltrados);
+    }
   };
 
-  const handleModal = () => {
+  const openModal = () => {
     setShowModal(!showModal);
   };
-  const handleSearch = query => {
-    setSearchQuery(query);
+
+  const handleClose = () => {
+    setShowModal(false);
   };
-   
-    return (
-      <div>
-        <div>
-          <hr style={{ marginBottom: '-15px', borderTop: '2px solid #B4D8E9' }} />
-          <h2 style={{ paddingLeft: '20px', marginTop: '15px', marginBottom: '-15px' }}>Nueva Venta</h2>
-          <hr style={{ borderTop: '2px solid #B4D8E9' }} />
-        </div>
-  
-        <div className="container">
-  
-          <br />
-  
-          {/* <!-- TABLAS --> */}
-          <Buscador action={handleModal} handleSearch={handleSearch} />
-         
-          <MDBTable small responsive className="table table-striped table-hover border-black " style={{
-            border: '1px solid black',
-            overflow: 'scroll',
-            maxHeight: '100px',
-          }} id="myTable"
-          >
-            <MDBTableHead  style={{
-            position: 'sticky',
-            top: '0' }}>
-              <tr>
-                <th scope="col" style={{ position: 'sticky',top: '0' }}>Nombre</th>
-                <th scope="col" style={{ position: 'sticky',top: '0' }}>Detalles</th>
-                <th scope="col" style={{ position: 'sticky',top: '0' }}>Categoria</th>
-                <th scope="col" style={{ position: 'sticky',top: '0' }}>Precio</th>
-                <th scope="col" style={{ position: 'sticky',top: '0' }}>Stock</th>
-                <th scope="col" style={{ position: 'sticky',top: '0' }}>Cantidad</th>
-                <th scope="col" style={{ position: 'sticky',top: '0' }}>Agregar Producto</th>
-              </tr>
-            </MDBTableHead>
-            <MDBTableBody>
-                <tr>
-                    <td>1</td>
-                    <td>Item 1</td>
-                    <td>Description of Item 1</td>
-                    <td>1</td>
-                    <td>Item 1</td>
-                    <td>Description of Item 1</td>
-                    <td><Button variant="secondary" >Agregar</Button></td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Item 2</td>
-                    <td>Description of Item 2</td>
-                    <td>1</td>
-                    <td>Item 1</td>
-                    <td>Description of Item 1</td>
-                    <td><Button variant="secondary" >Agregar</Button></td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Item 3</td>
-                    <td>Description of Item 3</td>
-                    <td>1</td>
-                    <td>Item 1</td>
-                    <td>Description of Item 1</td>
-                    <td><Button variant="secondary" >Agregar</Button></td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Item 3</td>
-                    <td>Description of Item 3</td>
-                    <td>1</td>
-                    <td>Item 1</td>
-                    <td>Description of Item 1</td>
-                    <td><Button variant="secondary" >Agregar</Button></td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Item 3</td>
-                    <td>Description of Item 3</td>
-                    <td>1</td>
-                    <td>Item 1</td>
-                    <td>Description of Item 1</td>
-                    <td><Button variant="secondary" >Agregar</Button></td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Item 3</td>
-                    <td>Description of Item 3</td>
-                    <td>1</td>
-                    <td>Item 1</td>
-                    <td>Description of Item 1</td>
-                    <td><Button variant="secondary" >Agregar</Button></td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Item 3</td>
-                    <td>Description of Item 3</td>
-                    <td>1</td>
-                    <td>Item 1</td>
-                    <td>Description of Item 1</td>
-                    <td><Button variant="secondary" >Agregar</Button></td>
-                </tr>
-            </MDBTableBody>
-          </MDBTable >
-          <div>
-          <hr style={{ marginBottom: '-15px', borderTop: '2px solid #B4D8E9' }} />
-          <h2 style={{ paddingLeft: '20px', marginTop: '15px', marginBottom: '-15px' }}>Productos Seleccionados</h2>
-          <hr style={{ borderTop: '2px solid #B4D8E9' }} />
-          <MDBTable small responsive className="table table-striped table-hover border-black " style={{
-            border: '1px solid black',
-            overflow: 'scroll',
-            maxHeight: '100px',
-          }} id="myTable"
-          >
-            <MDBTableHead  style={{
-            position: 'sticky',
-            top: '0' }}>
-              <tr>
-                <th scope="col" style={{ position: 'sticky',top: '0' }}>Nombre</th>
-                <th scope="col" style={{ position: 'sticky',top: '0' }}>Detalles</th>
-                <th scope="col" style={{ position: 'sticky',top: '0' }}>Categoria</th>
-                <th scope="col" style={{ position: 'sticky',top: '0' }}>Precio</th>
-                <th scope="col" style={{ position: 'sticky',top: '0' }}>Cantidad</th>
-                <th scope="col" style={{ position: 'sticky',top: '0' }}>Iva%</th>
-                <th scope="col" style={{ position: 'sticky',top: '0' }}>Total</th>
-                <th scope="col" style={{ position: 'sticky',top: '0' }}>Otros</th>
-              </tr>
-            </MDBTableHead>
-            <MDBTableBody>
-                <tr>
-                    <td>1</td>
-                    <td>Item 1</td>
-                    <td>Description of Item 1</td>
-                    <td>50.000</td>
-                    <td>1</td>
-                    <td>5.000</td>
-                    <td>55.000</td>
-                    <td><FaTrash/></td>
-                </tr>
-            </MDBTableBody>
-          </MDBTable >
-          <div style={{ border: '1px solid black', borderRadius:'7px', padding: '5px' }}>
-            <span style={{ fontWeight: 'bold' }}>Monto Total:</span> <span style={{ float: 'right' }}>55.000</span>
-          </div>
-          <div style={{ marginTop: '20px', display: 'flex', justifyContent: 'space-between' }}>
-            <Button variant="danger">Anular Venta</Button>
-            <Button variant="success">Confirmar Venta</Button>
-          </div>    
 
+  const agregarAlCarrito = (productoAgregado) => {
+    let productoYaEnCarrito = productosSeleccionados.find((producto) => producto.id === productoAgregado.id);
 
-        </div>
-        </div>
-      </div >
+    if (productoYaEnCarrito) {
+      // Si el producto ya está en el carrito, aumenta su cantidad
+      setCantidadProducto({
+        ...cantidadProducto,
+        [productoAgregado.id]: cantidadProducto[productoAgregado.id] - 1,
+      });
+    } else {
+      // Si el producto no está en el carrito, añádelo
+      setProductoSeleccionados([...productosSeleccionados, productoAgregado]);
+      setCantidadProducto({ ...cantidadProducto, [productoAgregado.id]: 1 });
+    }
+  };
+
+  const cambiarCantidadProducto = (idProducto, nuevaCantidad) => {
+    setProductoSeleccionados(
+      productosSeleccionados.map((producto) => {
+        if (producto.id === idProducto) {
+          return { ...producto, cantidad: nuevaCantidad };
+        }
+        return producto;
+      })
     );
-}
+  };
 
-export default VentaProductos
+  const eliminarProducto = (idProducto) => {
+    setProductoSeleccionados(productosSeleccionados.filter((producto) => producto.id !== idProducto));
+  };
+
+  const formatearPrecio = (precio) => {
+    return precio.toFixed(0);
+  };
+
+  let total = productosSeleccionados.reduce(
+    (total, producto) => total + producto.precioUnitario * (1 + 0.05) * producto.cantidad,
+    0
+  );
+
+  return (
+    <Container className="w-75 mt-4">
+      <Row className="mb-4">
+        <Col>
+          <h2>Proveedor: {(proveedor && proveedor.nombreEmpresa) || null}</h2>
+        </Col>
+      </Row>
+      <Row>
+        {/* Resto del código */}
+      </Row>
+      <Row className="mb-4">
+        <Row>
+          <Col>
+            <h2>Venta de Productos</h2>
+            <BuscadorCompraProductos handleSearch={handleSearch} action={openModal} />
+          </Col>
+        </Row>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Detalles</th>
+              <th>Costo</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* Aca va a ir un map de los productos */}
+            {productosFiltrados.map((producto) => (
+              <tr id={producto.id}>
+                <td> {producto.nombre}</td>
+                <td> {producto.descripcionTipoProducto}</td>
+                <td> {producto.precioUnitario}</td>
+                <td>
+                  <button onClick={() => agregarAlCarrito(producto)}>Añadir al carrito</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Row>
+      <Row className="mb-4">
+        <Col>
+          <h2>Productos Seleccionados</h2>
+          <Table striped bordered hover id="carrito">
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Detalles</th>
+                <th>Costo</th>
+                <th>Cantidad</th>
+                <th>IVA 5%</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Aca se cargan los elemento del carrito */}
+              {productosSeleccionados.map((producto) => (
+                <tr id={producto.id}>
+                  <td> {producto.nombre}</td>
+                  <td> {producto.descripcionTipoProducto}</td>
+                  <td> {formatearPrecio(producto.precioUnitario)}</td>
+                  <td>
+                    <input
+                      type="number"
+                      value={cantidadProducto[producto.id]}
+                      onChange={(e) => {
+                        const nuevaCantidad = e.target.value;
+                        setCantidadProducto({ ...cantidadProducto, [producto.id]: nuevaCantidad });
+                        cambiarCantidadProducto(producto.id, nuevaCantidad);
+                      }}
+                      min={1}
+                    />
+                    <button onClick={() => eliminarProducto(producto.id)}>Eliminar</button>
+                  </td>
+                  <td> {formatearPrecio(producto.precioUnitario * producto.cantidad * 0.05)}</td>
+                  <td> {formatearPrecio(producto.precioUnitario * (1 + 0.05) * producto.cantidad)}</td>
+                </tr>
+              ))}
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colSpan="5">Total</td>
+                <td>{/* Aca se calcula el total de todos los elementos del carrito */ formatearPrecio(total)}</td>
+              </tr>
+            </tfoot>
+          </Table>
+        </Col>
+      </Row>
+      <Row>
+        <Col style={{ justifyContent: 'center', display: 'flex' }}>
+          <Button variant="success">Confirmar Venta</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default VentaProductos;
+
