900885d5ddc8131420be2fb77f01490f6a0c5602
lamas <luz.lamas2019@fiuni.edu.py>
2023-06-12 17:12:47 -0400

    actualizacion lamas

---
+++ Models/DTOs/Outgoing/CajaAgregarDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeluqueriaWebApi.Models.DTOs.Outgoing
+{
+public class CajaAgregarDto
+{
+    public string Nombre { get; set; }
+    public string Clave { get; set; }
+    public decimal MontoApertura { get; set; }
+}
+}
